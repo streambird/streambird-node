f61999b138ff66e639793fb56873c3e0a21cfa7c
Quang Huynh <quanghuynh@gmail.com>
2022-04-28 17:01:47 -0400

    cleanup

--- test/testHelpers.ts
+++
@@ -1,6 +0,0 @@
-import nock from 'nock';
-import { Streambird } from "streambird-node";
-
-export const streambird = new Streambird({ apiToken: 'sk_dev_rM2H4PXf5rGmOmrhnGU36WWZ4XzFNoMr6WlvVIeWKA2pJlOq' });
-
-export const createNock = (): nock.Scope => nock('https://api-staging.streambird.io/v1')
